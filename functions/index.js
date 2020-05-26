06c6f44b9eaa74b9db53509033eff163ab273319
Evan Wappel <evanwappel@evans-mbp.lan>
2020-05-26 15:32:45 -0700

    sync

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,9 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
